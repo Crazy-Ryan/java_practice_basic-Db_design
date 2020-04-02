d1fdc3474f8ea979438f69b8de3d459246ffc558
Ryan.Wang <ranwang1995@outlook.com>
2020-04-02 21:12:48 +0800

    [WangRan] feat: add equals and hashCode methods to Teacher

--- src/main/java/entities/Teacher.java
+++ src/main/java/entities/Teacher.java
@@ -1,5 +1,7 @@
 package entities;
 
+import java.util.Objects;
+
 public class Teacher {
     private int id;
     private int name;
@@ -15,5 +17,18 @@ public class Teacher {
 
     public int getName() {
         return name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Teacher teacher = (Teacher) o;
+        return id == teacher.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
     }
 }
