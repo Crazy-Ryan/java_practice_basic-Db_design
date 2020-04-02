2b4e113431d4e57aa86c33d991d0b5d8f0eda904
Ryan.Wang <ranwang1995@outlook.com>
2020-04-02 21:12:11 +0800

    [WangRan] feat: add equals and hashCode methods to Student

--- src/main/java/entities/Student.java
+++ src/main/java/entities/Student.java
@@ -1,5 +1,7 @@
 package entities;
 
+import java.util.Objects;
+
 public class Student {
     private int id;
     private String name;
@@ -27,5 +29,18 @@ public class Student {
 
     public String getGender() {
         return gender;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return id == student.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
     }
 }
