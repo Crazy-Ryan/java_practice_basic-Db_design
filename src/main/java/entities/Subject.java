0f49f9175426e3770b72a0d18eac5e6cadfdf18a
Ryan.Wang <ranwang1995@outlook.com>
2020-04-02 21:13:23 +0800

    [WangRan] feat: add equals and hashCode methods to Subject

--- src/main/java/entities/Subject.java
+++ src/main/java/entities/Subject.java
@@ -1,5 +1,7 @@
 package entities;
 
+import java.util.Objects;
+
 public class Subject {
     private int id;
     private String name;
@@ -21,5 +23,18 @@ public class Subject {
 
     public int getTeacherId() {
         return teacherId;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Subject subject = (Subject) o;
+        return id == subject.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
     }
 }
